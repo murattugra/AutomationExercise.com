c3199a385e19f8c9abd540cd1944861fd38c9336
Hasan F <hfidan006@gmail.com>
2022-06-15 20:02:30 +0200

    feature_olusturuldu

---
+++ src/test/java/pages/HF_RegisterPage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class HF_RegisterPage {
+
+
+    public HF_RegisterPage() {PageFactory.initElements(Driver.getDriver(),this);}
+
+
+
+
+
+
+
+}
+
