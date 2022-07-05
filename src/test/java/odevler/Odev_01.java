9de015d7f18bb8899a862f76206fad77e3b5feb2
Hasan F <hfidan006@gmail.com>
2022-07-05 12:19:19 +0200

    odevler

---
+++ src/test/java/odevler/Odev_01.java
@@ -0,0 +1,30 @@
+package odevler;
+
+public class Odev_01 {
+    public static void main(String[] args) {
+
+        /*
+         * Kullanicidan ay olarak 1 ile 12 arasindaki bir sayiyi alan,
+         * sayiyi ay isimleri olarak yazdiran bir method olusturun
+         *
+         * INPUT : 11 OUTPUT: Kasim
+         * INPUT :  5 OUTPUT: Mayis
+         * INPUT : 15 OUTPUT: Lutfen 1 ile 12 arasinda bir numara girin
+         *
+         * Switch Case kullanilabilir.
+         */
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
